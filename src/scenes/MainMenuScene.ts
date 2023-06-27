3326b3d1368d20c4355115d3659a7aa0ffc94d72
Nguyễn Phan Anh Tuấn <tuan.nguyenphananh@gmail.com>
2023-06-27 13:35:05 +0700

    Update MainMenuScene.ts

--- src/scenes/MainMenuScene.ts
+++ src/scenes/MainMenuScene.ts
@@ -8,19 +8,39 @@ export default class MainMenuScene extends Phaser.Scene {
     private pauseBtn: Button
     private customizeBtn: Button
 
+    private title: Phaser.GameObjects.Image
+    private help: Phaser.GameObjects.Sprite
+
     constructor() {
         super('main-menu')
         GameManager.emitter.on('game-state-changed', this.onGameStateChanged)
     }
 
     create() {
-        const title = this.add
+        this.createTitle()
+        this.createHelpAnimation()
+        this.createPauseButton()
+        this.createCustomizeButton()
+        this.setupInputEvents()
+        new StarManager(this)
+    }
+
+    private createTitle() {
+        this.title = this.add
             .image(CANVAS_WIDTH * 0.5, -CANVAS_HEIGHT * 0.25, 'title')
             .setScale(0.5)
 
-        const help = this.add.sprite(CANVAS_WIDTH * 0.2, CANVAS_HEIGHT * 0.88, 'help').setScale(0.6)
+        this.tweens.add({
+            targets: this.title,
+            y: { value: CANVAS_HEIGHT * 0.25, duration: 500 },
+            ease: 'Quad.out',
+        })
+    }
 
-        help.anims.create({
+    private createHelpAnimation() {
+        this.help = this.add.sprite(CANVAS_WIDTH * 0.2, CANVAS_HEIGHT * 0.88, 'help').setScale(0.6)
+
+        this.help.anims.create({
             key: 'idle',
             frames: this.anims.generateFrameNames('help', {
                 start: 0,
@@ -30,65 +50,71 @@ export default class MainMenuScene extends Phaser.Scene {
             repeat: -1,
         })
 
-        help.play('idle')
+        this.help.play('idle')
+    }
 
+    private createPauseButton() {
         this.pauseBtn = new Button({
             scene: this,
             x: CANVAS_WIDTH * 0.06,
             y: CANVAS_HEIGHT * 0.035,
             texture: 'pause-btn',
             scale: 0.25,
-            pointerUpCallback: () => {
-                if (GameManager.getCurrentState() === GameState.PLAYING) {
-                    GameManager.updateGameState(GameState.PAUSE)
-                    this.scene.sleep().pause('game').launch('pause')
-                }
-            },
+            pointerUpCallback: this.handlePauseButtonClick,
         }).setAlpha(0)
+    }
 
+    private createCustomizeButton() {
         this.customizeBtn = new Button({
             scene: this,
             x: CANVAS_WIDTH * 0.7,
             y: CANVAS_HEIGHT * 0.85,
             texture: 'customize-mainmenu-btn',
             scale: 0.4,
-            pointerDownCallback: () => {
-                GameManager.updateGameState(GameState.CUSTOMIZE)
-                this.scene.stop('game').stop('result').start('customize')
-            },
+            pointerDownCallback: this.handleCustomizeButtonClick,
         })
+    }
 
-        this.add.tween({
-            targets: title,
-            y: { value: CANVAS_HEIGHT * 0.25, duration: 500 },
-            ease: 'Quad.out',
-        })
-
+    private setupInputEvents() {
         this.input.on('pointerdown', () => {
             if (GameManager.getCurrentState() === GameState.READY) {
                 GameManager.updateGameState(GameState.PLAYING)
-                help.stop()
-
-                this.add.tween({
-                    targets: [title, help, this.customizeBtn],
-                    alpha: { value: 0, duration: 500 },
-                    ease: 'Quad.out',
-                })
-
-                this.add.tween({
-                    targets: this.pauseBtn,
-                    alpha: { value: 1, duration: 500 },
-                    ease: 'Quad.out',
-                })
+                this.handleGameStart()
             }
         })
+    }
 
-        new StarManager(this)
+    private handlePauseButtonClick = () => {
+        if (GameManager.getCurrentState() === GameState.PLAYING) {
+            GameManager.updateGameState(GameState.PAUSE)
+            this.scene.sleep().pause('game').launch('pause')
+        }
+    }
+
+    private handleCustomizeButtonClick = () => {
+        GameManager.updateGameState(GameState.CUSTOMIZE)
+        this.scene.stop('game').stop('result').start('customize')
+    }
+
+    private handleGameStart() {
+        this.tweens.add({
+            targets: [this.title, this.help, this.customizeBtn],
+            alpha: { value: 0, duration: 500 },
+            ease: 'Quad.out',
+        })
+
+        this.tweens.add({
+            targets: this.pauseBtn,
+            alpha: { value: 1, duration: 500 },
+            ease: 'Quad.out',
+        })
+
+        this.help.stop()
     }
 
     private onGameStateChanged = (gameState: GameState) => {
         if (gameState === GameState.GAME_OVER) {
-            this.add.tween({
+            this.tweens.add({
                 targets: this.pauseBtn,
                 alpha: { value: 0, duration: 500 },
                 ease: 'Quad.out',
