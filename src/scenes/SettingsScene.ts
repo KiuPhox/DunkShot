612a91406bb363e9bb715b41165af91d76025c13
Nguyễn Phan Anh Tuấn <tuan.nguyenphananh@gmail.com>
2023-06-27 17:49:33 +0700

    Update SettingsScene.ts

--- src/scenes/SettingsScene.ts
+++ src/scenes/SettingsScene.ts
@@ -19,6 +19,7 @@ export default class SettingsScene extends Phaser.Scene {
         this.createBackButton()
         this.createTexts()
         this.createSwitchButtons()
+        this.createClearDataButton()
     }
 
     private createBackButton() {
@@ -50,31 +51,44 @@ export default class SettingsScene extends Phaser.Scene {
     }
 
     private createTexts() {
-        this.addText(CANVAS_WIDTH * 0.1, CANVAS_HEIGHT * 0.4, 'SOUNDS')
-        this.addText(CANVAS_WIDTH * 0.1, CANVAS_HEIGHT * 0.5, 'VIBRATION')
-        this.addText(CANVAS_WIDTH * 0.1, CANVAS_HEIGHT * 0.6, 'NIGHT MODE')
+        this.addText(CANVAS_WIDTH * 0.1, CANVAS_HEIGHT * 0.3, 'SOUNDS')
+        this.addText(CANVAS_WIDTH * 0.1, CANVAS_HEIGHT * 0.4, 'VIBRATION')
+        this.addText(CANVAS_WIDTH * 0.1, CANVAS_HEIGHT * 0.5, 'NIGHT MODE')
     }
 
     private addText(x: number, y: number, text: string) {
         this.add.bitmapText(x, y, 'objet', text, 50).setTint(0xc1c1c1).setDepth(-3)
     }
 
-    private createSwitchButtons() {
+    private createClearDataButton(): void {
+        new Button({
+            scene: this,
+            x: CANVAS_HEIGHT * 0.3,
+            y: CANVAS_HEIGHT * 0.7,
+            texture: 'cleardata-btn',
+            scale: 0.5,
+            pointerUpCallback: () => {
+                Storage.deleteAll()
+            },
+        })
+    }
+
+    private createSwitchButtons(): void {
         this.soundBtn = this.createSwitchButton(
             CANVAS_WIDTH * 0.7,
-            CANVAS_HEIGHT * 0.4,
+            CANVAS_HEIGHT * 0.3,
             STORAGE_KEY.SOUND
         ).setActive(Storage.getString(STORAGE_KEY.SOUND) === 'true')
 
         this.vibrationBtn = this.createSwitchButton(
             CANVAS_WIDTH * 0.7,
-            CANVAS_HEIGHT * 0.5,
+            CANVAS_HEIGHT * 0.4,
             STORAGE_KEY.VIBRATION
         ).setActive(Storage.getString(STORAGE_KEY.VIBRATION) === 'true')
 
         this.nightModeBtn = this.createSwitchButton(
             CANVAS_WIDTH * 0.7,
-            CANVAS_HEIGHT * 0.6,
+            CANVAS_HEIGHT * 0.5,
             STORAGE_KEY.NIGHT_MODE
         ).setActive(Storage.getString(STORAGE_KEY.NIGHT_MODE) === 'true')
     }
