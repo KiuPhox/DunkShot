5b7234cb05607624abbe95281e2e5e3cf318f494
Nguyễn Phan Anh Tuấn <tuan.nguyenphananh@gmail.com>
2023-07-03 14:04:01 +0700

    Update Level.ts

--- src/constant/Level.ts
+++ src/constant/Level.ts
@@ -2,4 +2,4 @@ export const STAR_CHANCES = 20
 export const MOVEABLE_BASKET_CHANCES = 10
 export const MINI_WALL_CHANCES = 10
 export const BOUNCER_CHANCES = 10
-export const SHIELD_CHANCES = 30
+export const SHIELD_CHANCES = 10
